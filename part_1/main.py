5871ed629c53c9f7fecf3d96bfa2f86c3255ab63
NikitaKolotushkin <nikitaplay1000@gmail.com>
2022-02-11 17:56:45 +0400

    Схемы для представителей разных профессий

--- part_1/main.py
+++ part_1/main.py
@@ -10,8 +10,14 @@ app = Flask(__name__)
 @app.route('/<title_>')
 @app.route('/index/<title_>')
 def index(title_):
-    return render_template('base.html', title=title_)
+    return render_template('base.html', title=title_, img='')
+
+
+@app.route('/training/<prof_>')
+def training(prof_):
+    return render_template('training.html', prof=prof_)
 
 
 if __name__ == '__main__':
+    app.debug = 1
     app.run(port=8080, host='127.0.0.1')
